e19f62697ac806994bd80a3f0eb8af70b2e0ca26
David Liu <davidbliu@gmail.com>
2016-03-02 16:44:51 -0800

    add heroku env setting script

---
+++ heroku_deploy.sh
@@ -0,0 +1,3 @@
+heroku config:set GOOGLE_CLIENT_ID=158145275272-lj3m0j741dj9fp50rticq48vtrfu59jj.apps.googleusercontent.com
+heroku config:set GOOGLE_CLIENT_SECRET=XvvJaFO2t2lD0mEzNdya6NgT
+heroku restart
