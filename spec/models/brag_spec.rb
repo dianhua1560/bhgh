00b68f07e94228c490deff37b84e938f831cb59b
David Liu <davidbliu@gmail.com>
2016-03-30 16:19:32 -0700

    fix rspec for brags

--- spec/models/brag_spec.rb
+++ spec/models/brag_spec.rb
@@ -4,7 +4,11 @@ require 'rails_helper'
 describe Brag do
 	describe 'brag' do
 		it 'creates brag' do
-			Brag.create()
+			Brag.create(
+				title: 'valid title',
+				author:'a@gmail.com',
+				subject:'s@gmail.com',
+				body: 'body')
 		end
 	end
 end
