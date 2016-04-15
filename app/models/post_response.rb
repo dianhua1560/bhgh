3390e6c819bde46e924444efa1131c75b9ec4c4e
Mike Chen <mchen599@berkeley.edu>
2016-04-14 18:52:29 -0700

    asdf

--- app/models/post_response.rb
+++ app/models/post_response.rb
@@ -7,7 +7,8 @@ class PostResponse < ActiveRecord::Base
 			author: self.author,
 			body: self.body,
 			gravatar: self.gravatar,
-			created_at: self.created_at
+			created_at: self.created_at,
+			timestamp: self.timestamp
 		}
 	end
 
@@ -16,6 +17,8 @@ class PostResponse < ActiveRecord::Base
 		gravatar_id = Digest::MD5.hexdigest(email.downcase)
 		return "http://gravatar.com/avatar/#{gravatar_id}.png"
 	end
-	
 
+	def timestamp
+		self.created_at.in_time_zone('Pacific Time (US & Canada)').strftime("%B %d, %Y at %r")
+	end	
 end
