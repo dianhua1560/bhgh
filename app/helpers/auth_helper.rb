6d0340aa01f7598ad99d28fbde15187ea03db4d1
Allen Chen <chenallen2012@gmail.com>
2016-04-29 18:55:23 -0700

    passing cuke/rspec, before merging to master

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -2,7 +2,7 @@ module AuthHelper
     def myEmail
     	# return "scholar@gmail.com"
         # return 'davidbliu@gmail.com'
-    	return "chenallen2012@gmail.com"
+    	# return "chenallen2012@gmail.com"
         cookies[:email] ? cookies[:email] : nil
     end
 
