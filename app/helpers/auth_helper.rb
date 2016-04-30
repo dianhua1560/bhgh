3fa1aa172b316e1a3f3e049205cd733045ab2c45
Allen Chen <chenallen2012@gmail.com>
2016-04-29 18:39:45 -0700

    Added more info; link to homepage and fb

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,8 +1,8 @@
 module AuthHelper
     def myEmail
     	# return "scholar@gmail.com"
-            #return 'davidbliu@gmail.com'
-    	# return "chenallen2012@gmail.com"
+        # return 'davidbliu@gmail.com'
+    	return "chenallen2012@gmail.com"
         cookies[:email] ? cookies[:email] : nil
     end
 
