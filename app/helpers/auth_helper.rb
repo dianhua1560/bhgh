d806426de46e6f786a85d29cce90b029b5b3c8b1
David Liu <davidbliu@gmail.com>
2016-04-29 17:16:47 -0700

    merge new styles

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,7 +1,7 @@
 module AuthHelper
     def myEmail
     	# return "scholar@gmail.com"
-    	# return 'davidbliu@gmail.com'
+            #return 'davidbliu@gmail.com'
     	# return "chenallen2012@gmail.com"
         cookies[:email] ? cookies[:email] : nil
     end
